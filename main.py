0eed652ee3bbeba0938e114a6ec3c8ef297c9b8e
Aime Malaika <aimemalaika1995@gmail.com>
2021-01-13 12:54:42 +0200

    image processiong lesson one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image,ImageFilter
 
-image = Image.open('./Pokedex/4.5 pikachu.jpg.jpg')
+# image = Image.open('./Pokedex/4.5 pikachu.jpg.jpg')
 
 # filtered_image = image.filter(ImageFilter.SMOOTH)
 
@@ -11,7 +11,18 @@ image = Image.open('./Pokedex/4.5 pikachu.jpg.jpg')
 # resize = convert_image.resize((30,300))
 # resize.save('gray.png','png')
 
-convert_image = image.convert('L')
-box = (100,100,400,400)
-region = convert_image.crop(box)
-regiongit .save('gray.png','png')
+# convert_image = image.convert('L')
+# box = (100,100,400,400)
+# region = convert_image.crop(box)
+# region.save('gray.png','png')
+
+
+#resizing image
+
+# image = Image.open('./Pokedex/6.1 astro.jpg.jpg')
+# newImage =image.thumbnail((400,400))
+# newImage.save('thumb.jpg')
+
+image = Image.open('./Pokedex/6.1 astro.jpg.jpg')
+image.thumbnail((400,400))
+image.save('thumb.jpg')
